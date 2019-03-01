efa4002010ab7a88ce5c2087cd21bc71251f605a
jevan1000 <33735197+jevan1000@users.noreply.github.com>
2019-02-28 16:32:32 -0800

    Final Project Submission
    - Added README.txt
    - Added Report.pdf
    - Slight changes to main.py

---
+++ README.txt
@@ -0,0 +1,74 @@
+----------------------------------------------------------
+.:GROUP MEMBERS:.
+----------------------------------------------------------
+Made by: Jevan, and Juan
+
+
+----------------------------------------------------------
+.:REQUIREMENTS:.
+----------------------------------------------------------
+- Built using python 3.7, PyCharm (Version 2018.3.5) is recommended
+- matplotlib is required (Version 3.0.2)
+-- matplotlib.pyplot
+-- matplotlib.patches
+
+
+----------------------------------------------------------
+.:EXPECTED OUTPUTS BELOW:.
+----------------------------------------------------------
+*Note to use 'Scatter plot mode', matplotlib must be installed!
+Below is our programs output for 'User testing mode'
+
+---------------------------------------------
+.:TEST PROGRAM OUTPUT:.
+---------------------------------------------
+[1] Enter 'User testing mode'
+[2] Enter 'Scatter plot mode'
+[3] Exit
+Option: 1
+
+Mode: User testing
+[1] Task 1
+[2] Task 2
+[3] Task 3
+[4] Go Back
+[5] Exit
+Option: 1
+
+Task 1: 
+Enter positive value for 'n': 4
+Average number of Modulo Divisions GCD is: 1.25
+Average number of Divisions GCD is: 2.75
+
+Mode: User testing
+[1] Task 1
+[2] Task 2
+[3] Task 3
+[4] Go Back
+[5] Exit
+Option: 2
+
+Task 2: 
+Enter positive value for 'k': 16
+Fibonacci Sequence: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+Total number of Modulo Divisions is: 92
+Fibonacci value of k: 987
+Number of Modulo Divisions for the value of k: 14
+Time Taken to output result: 0.0
+
+Mode: User testing
+[1] Task 1
+[2] Task 2
+[3] Task 3
+[4] Go Back
+[5] Exit
+Option: 3
+
+Task 3: 
+Enter positive (m > 1) value for 'm': 15
+Enter positive (n > 1) value for 'n': 9
+Sieve's Algorithm based on 'm': [2, 3, 5, 7, 11, 13]
+Sieve's Algorithm based on 'n': [2, 3, 5, 7]
+Prime factors for 'm': [3, 5]
+Prime factors for 'n': [3, 3]
+Middle-school GCD(15,9) = 3 = 3
--- main.py
+++ main.py
@@ -147,11 +147,6 @@ def gcd_middle(m, n):
         max_value = max(len(m_primefactors), len(n_primefactors))
         #  max_value = len(m_primefactors) + len(n_primefactors)
     except:
-        print("ERROR!, DEBUG VALUES BELOW:")
-        print("value of m:", m)
-        print("value of n:", n)
-        print("prime factors of m:", m_primefactors)
-        print("prime factors of n:", n_primefactors)
         return result, total, counter, 0
 
     for i in range(max(len(m_primefactors), len(n_primefactors))):
@@ -190,259 +185,254 @@ def fib_seq(k):
 """ 
 Main program loop
 """
-print("")
-try:
-    while True:
+while True:
 
-        print("[1] Enter 'User testing mode'")
-        print("[2] Enter 'Scatter plot mode'")
-        print("[3] Exit")
+    print("[1] Enter 'User testing mode'")
+    print("[2] Enter 'Scatter plot mode'")
+    print("[3] Exit")
 
-        option = int(input("Option: "))
-        print("")
-
-        if option == 1:
-            while True:
-                print("Mode: User testing")
-                print("[1] Task 1")
-                print("[2] Task 2")
-                print("[3] Task 3")
-                print("[4] Go Back")
-                print("[5] Exit")
-
-                option = int(input("Option: "))
-                print("")
-
-                if option == 1:
-                    print("Task 1: ")
-                    n = int(input("Enter positive value for 'n': "))
-                    print("Average number of Modulo Divisions GCD is:", gcd_avg(n))
-                    print("Average number of Divisions GCD is:", ica_avg(n))
-                    print("")
-
-                elif option == 2:
-                    print("Task 2: ")
-                    k = int(input("Enter positive value for 'k': "))
-                    start = time.time()
-                    fib = fib_seq(k)
-                    print("Fibonacci Sequence:", fib)
-                    total_Mod = 0
-                    for i in range(0, k-2):
-                        total_Mod += gcd_count(fib[i+1], fib[i])
-                    end = time.time()
-                    print("Total number of Modulo Divisions is:", total_Mod)
-                    print("Time Taken to output result:", end - start)
-                    print("")
-
-                elif option == 3:
-                    print("Task 3: ")
-                    m = int(input("Enter positive value for 'm': "))
-                    n = int(input("Enter positive value for 'n': "))
-                    x = gcd_middle(m, n)[0]
-                    y = gcd_middle(m, n)[1]
-                    z = sieve(m)
-                    z2 = sieve(n)
-                    print("Sieve's Algorithm based on 'm':", z)
-                    print("Sieve's Algorithm based on 'n':", z2)
-                    print("Prime factors for 'm':", primefactors(m))
-                    print("Prime factors for 'n':", primefactors(n))
-                    print("Middle-school GCD(", m, ",", n, ") = ", sep="", end="")
-                    if len(x) != 0:
-                        for i in range(len(x)):
-                            if i == len(x)-1:
-                                print(x[i], end="")
-                            else:
-                                print(x[i], " * ", sep="", end="")
-                    else:
-                        print("1", end="")
-                    print(" =", y)
-                    print("")
-
-                elif option == 4:
-                    break
-
-                elif option == 5:
-                    print("Goodbye!\n")
-                    exit()
-
-                else:
-                    print("Invalid input, try again.\n")
-
-        elif option == 2:
-            while True:
-                print("Mode: Scatter plot")
-                print("[1] Task 1")
-                print("[2] Task 2")
-                print("[3] Task 3")
-                print("[4] Go Back")
-                print("[5] Exit")
-
-                option = int(input("Option: "))
-                print("")
-
-                if option == 1:
-                    print("Task 1: ")
-                    x = []
-                    y = []
-                    x1 = []
-                    y1 = []
-                    print("-> LOADING GRAPH <-")
-                    for i in range(1, 200):
-                        n = i
-                        n2 = i
-                        gcd = gcd_avg(n)
-                        ica = ica_avg(n2)
-                        x.append(n)
-                        y.append(gcd)
-                        x1.append(n2)
-                        y1.append(ica)
-
-                    print("-> GRAPH LOADED <-")
-                    print("Plotted Points Listed Below: ")
-                    print("Euclid Average's:", y)
-                    print("Consecutive Integer Average's:", y1)
-
-                    plt.plot(x, y, 'bs')
-                    plt.plot(x1, y1, 'ro')
-                    plt.axis([0, 100, 0, 10])
-                    blue_patch = mpatches.Patch(color='blue', label="Euclid's Algorithm")
-                    red_patch = mpatches.Patch(color='red', label='Consecutive Integer Algorithm')
-                    plt.legend(handles=[red_patch, blue_patch])
-                    plt.title("Average-Case Efficiencies Graph")
-                    plt.xlabel('Input Size "n"')
-                    plt.ylabel('Average Mod/Divisions')
-                    plt.show()
-
-                    x.clear()
-                    y.clear()
-                    print("")
-                    print("-> LOADING GRAPH <-")
-                    for i in range(1, 500):
-                        n = i
-                        gcd = gcd_avg(n)
-                        x.append(n)
-                        y.append(gcd)
-
-                    print("-> GRAPH LOADED <-")
-                    print("Input Size 'n':", x)
-                    print("Euclid Average's:", y)
-                    plt.plot(x, y, 'bs')
-                    plt.axis([0, 500, 0, 7])
-                    blue_patch = mpatches.Patch(color='blue', label="Euclid's Algorithm")
-                    plt.title("Average-Case Euclid's Efficiencies Graph")
-                    plt.xlabel('Input Size "n"')
-                    plt.ylabel('Average Mod')
-                    plt.show()
-
-                    x.clear()
-                    y.clear()
-                    print("")
-                    print("-> LOADING GRAPH <-")
-                    for i in range(1, 500):
-                        n = i
-                        gcd = ica_avg(n)
-                        x.append(n)
-                        y.append(gcd)
-
-                    print("-> GRAPH LOADED <-")
-                    print("Input Size 'n':", x)
-                    print("Consecutive Integer Average's:", y)
-                    plt.plot(x, y, 'bs')
-                    plt.axis([0, 500, 0, 500])
-                    red_patch = mpatches.Patch(color='red', label='Consecutive Integer Algorithm')
-                    plt.title("Average-Case Consecutive integer Efficiencies Graph")
-                    plt.xlabel('Input Size "n"')
-                    plt.ylabel('Average Divisions')
-                    plt.show()
-
-                    print("")
-
-                elif option == 2:
-                    print("Task 2: ")
-                    x = []
-                    y = []
-                    y1 = []
-                    k = 20
-                    fib = fib_seq(k)
-                    total_Mod = 0
-                    print("Plotted Points Listed Below:")
-                    for i in range(0, k-2):
-                        start = time.time()
-                        total_Mod = gcd_count(fib[i+1], fib[i])
-                        end = time.time()
-                        x.append(fib[i+1])
-                        y.append(total_Mod)
-                        y1.append(start - end)
-
-                    #  First Graph
-                    print("Modulo Division Plot Points:")
-                    print("Value's of 'm':", x)
-                    print("Number of Modulo Divisions:", y)
-                    plt.plot(x, y, 'bs')
-                    plt.axis([-500, 5000, 0, 20])
-                    plt.title("Worst-Case Efficiency For Euclid's Algorithm Graph")
-                    plt.xlabel('Value of "m"')
-                    plt.ylabel('Number of Modulo Divisions')
-                    plt.show()
-
-                    print("")
-
-                    #  Second Graph
-                    print("Time Based Plot Points:")
-                    print("Value's of 'm':", x)
-                    print("Time Taken:", y1)
-                    plt.plot(x, y1, 'ro')
-                    plt.axis([-500, 5000, 0, 0.0009])
-                    plt.title("Worst-Case Efficiency For Euclid's Algorithm Graph")
-                    plt.xlabel('Value of "m"')
-                    plt.ylabel('Time Taken')
-                    plt.show()
-
-                    print("")
-
-                elif option == 3:
-                    print("Task 3: ")
-                    size = 200
-                    x = []
-                    y = []
-
-                    print("-> LOADING GRAPH <-")
-                    for i in range(1, 1000):
-                        m = random.randint(2, 4000)
-                        n = random.randint(2, 4000)
-                        x_temp = gcd_middle(m, n)[3]
-                        y_temp = gcd_middle(m, n)[2]
-                        x.append(x_temp)
-                        y.append(y_temp)
-
-                    print("-> GRAPH LOADED <-")
-                    print("Max List Values:", x)
-                    print("Number of Comparisons:", y)
-                    plt.plot(x, y, 'ro')
-                    plt.axis([0, 12, 0, 31])
-                    plt.title("Common Factor Algorithm Complexity Graph")
-                    plt.xlabel('Max List Value')
-                    plt.ylabel('Number of Comparisons')
-                    plt.show()
-                    print("")
-
-                elif option == 4:
-                    break
-
-                elif option == 5:
-                    print("Goodbye!\n")
-                    exit()
-
-                else:
-                    print("Invalid input, try again.\n")
-
-        elif option == 3:
-            print("Goodbye!\n")
-            exit()
-        else:
-            print("Invalid input, try again.\n")
-
-except BaseException:
+    option = int(input("Option: "))
     print("")
-    print("-> OPPS! SOMETHING BAD HAPPEN, TRY RUNNING PROGRAM AGAIN! <-")
-    exit(999)
+
+    if option == 1:
+        while True:
+            print("Mode: User testing")
+            print("[1] Task 1")
+            print("[2] Task 2")
+            print("[3] Task 3")
+            print("[4] Go Back")
+            print("[5] Exit")
+
+            option = int(input("Option: "))
+            print("")
+
+            if option == 1:
+                print("Task 1: ")
+                n = int(input("Enter positive value for 'n': "))
+                print("Average number of Modulo Divisions GCD is:", gcd_avg(n))
+                print("Average number of Divisions GCD is:", ica_avg(n))
+                print("")
+
+            elif option == 2:
+                print("Task 2: ")
+                k = int(input("Enter positive value for 'k': "))
+                start = time.time()
+                fib = fib_seq(k)
+                print("Fibonacci Sequence:", fib)
+                total_Mod = 0
+                for i in range(0, k-2):
+                    total_Mod += gcd_count(fib[i+1], fib[i])
+                end = time.time()
+                print("Total number of Modulo Divisions is:", total_Mod)
+                print("Fibonacci value of k:", fib[k-1])
+                print("Number of Modulo Divisions for the value of k:", gcd_count(fib[k-1], fib[k-2]))
+                print("Time Taken to output result:", end - start)
+                print("")
+
+            elif option == 3:
+                print("Task 3: ")
+                m = int(input("Enter positive (m > 1) value for 'm': "))
+                n = int(input("Enter positive (n > 1) value for 'n': "))
+                x = gcd_middle(m, n)[0]
+                y = gcd_middle(m, n)[1]
+                z = sieve(m)
+                z2 = sieve(n)
+                print("Sieve's Algorithm based on 'm':", z)
+                print("Sieve's Algorithm based on 'n':", z2)
+                print("Prime factors for 'm':", primefactors(m))
+                print("Prime factors for 'n':", primefactors(n))
+                print("Middle-school GCD(", m, ",", n, ") = ", sep="", end="")
+                if len(x) != 0:
+                    for i in range(len(x)):
+                        if i == len(x)-1:
+                            print(x[i], end="")
+                        else:
+                            print(x[i], " * ", sep="", end="")
+                else:
+                    print("1", end="")
+                print(" =", y)
+                print("")
+
+            elif option == 4:
+                break
+
+            elif option == 5:
+                print("Goodbye!\n")
+                exit()
+
+            else:
+                print("Invalid input, try again.\n")
+
+    elif option == 2:
+        while True:
+            print("Mode: Scatter plot")
+            print("[1] Task 1")
+            print("[2] Task 2")
+            print("[3] Task 3")
+            print("[4] Go Back")
+            print("[5] Exit")
+
+            option = int(input("Option: "))
+            print("")
+
+            if option == 1:
+                print("Task 1: ")
+                x = []
+                y = []
+                x1 = []
+                y1 = []
+                print("-> LOADING GRAPH <-")
+                for i in range(1, 200):
+                    n = i
+                    n2 = i
+                    gcd = gcd_avg(n)
+                    ica = ica_avg(n2)
+                    x.append(n)
+                    y.append(gcd)
+                    x1.append(n2)
+                    y1.append(ica)
+
+                print("-> GRAPH LOADED <-")
+                print("Plotted Points Listed Below: ")
+                print("Euclid Average's:", y)
+                print("Consecutive Integer Average's:", y1)
+
+                plt.plot(x, y, 'bs')
+                plt.plot(x1, y1, 'ro')
+                plt.axis([0, 100, 0, 10])
+                blue_patch = mpatches.Patch(color='blue', label="Euclid's Algorithm")
+                red_patch = mpatches.Patch(color='red', label='Consecutive Integer Algorithm')
+                plt.legend(handles=[red_patch, blue_patch])
+                plt.title("Average-Case Efficiencies Graph")
+                plt.xlabel('Input Size "n"')
+                plt.ylabel('Average Mod/Divisions')
+                plt.show()
+
+                x.clear()
+                y.clear()
+                print("")
+                print("-> LOADING GRAPH <-")
+                for i in range(1, 500):
+                    n = i
+                    gcd = gcd_avg(n)
+                    x.append(n)
+                    y.append(gcd)
+
+                print("-> GRAPH LOADED <-")
+                print("Input Size 'n':", x)
+                print("Euclid Average's:", y)
+                plt.plot(x, y, 'bs')
+                plt.axis([0, 500, 0, 7])
+                blue_patch = mpatches.Patch(color='blue', label="Euclid's Algorithm")
+                plt.title("Average-Case Euclid's Efficiencies Graph")
+                plt.xlabel('Input Size "n"')
+                plt.ylabel('Average Mod')
+                plt.show()
+
+                x.clear()
+                y.clear()
+                print("")
+                print("-> LOADING GRAPH <-")
+                for i in range(1, 500):
+                    n = i
+                    gcd = ica_avg(n)
+                    x.append(n)
+                    y.append(gcd)
+
+                print("-> GRAPH LOADED <-")
+                print("Input Size 'n':", x)
+                print("Consecutive Integer Average's:", y)
+                plt.plot(x, y, 'bs')
+                plt.axis([0, 500, 0, 500])
+                red_patch = mpatches.Patch(color='red', label='Consecutive Integer Algorithm')
+                plt.title("Average-Case Consecutive integer Efficiencies Graph")
+                plt.xlabel('Input Size "n"')
+                plt.ylabel('Average Divisions')
+                plt.show()
+
+                print("")
+
+            elif option == 2:
+                print("Task 2: ")
+                x = []
+                y = []
+                y1 = []
+                k = 1000
+                fib = fib_seq(k)
+                total_Mod = 0
+                print("Plotted Points Listed Below:")
+                for i in range(0, k-2):
+                    start = time.time()
+                    total_Mod = gcd_count(fib[i+1], fib[i])
+                    end = time.time()
+                    x.append(fib[i+1])
+                    y.append(total_Mod)
+                    y1.append((end-start))
+
+                #  First Graph
+                print("Modulo Division Plot Points:")
+                print("Value's of 'm':", x)
+                print("Number of Modulo Divisions:", y)
+                plt.plot(x, y, 'bs')
+                plt.axis([-500, 5000, 0, 20])
+                plt.title("Worst-Case Efficiency For Euclid's Algorithm Graph")
+                plt.xlabel('Value of "m"')
+                plt.ylabel('Number of Modulo Divisions')
+                plt.show()
+
+                print("")
+
+                #  Second Graph
+                print("Time Based Plot Points:")
+                print("Value's of 'm':", x)
+                print("Time Taken:", y1)
+                plt.plot(x, y1, 'ro')
+                plt.axis([-500, 5000, 0, 9.1457672119140625e-06])
+                plt.title("Worst-Case Efficiency For Euclid's Algorithm Graph")
+                plt.xlabel('Value of "m"')
+                plt.ylabel('Time Taken')
+                plt.show()
+
+                print("")
+
+            elif option == 3:
+                print("Task 3: ")
+                size = 200
+                x = []
+                y = []
+
+                print("-> LOADING GRAPH <-")
+                for i in range(1, 1000):
+                    m = random.randint(2, 4000)
+                    n = random.randint(2, 4000)
+                    x_temp = gcd_middle(m, n)[3]
+                    y_temp = gcd_middle(m, n)[2]
+                    x.append(x_temp)
+                    y.append(y_temp)
+
+                print("-> GRAPH LOADED <-")
+                print("Max List Values:", x)
+                print("Number of Comparisons:", y)
+                plt.plot(x, y, 'ro')
+                plt.axis([0, 12, 0, 31])
+                plt.title("Common Factor Algorithm Complexity Graph")
+                plt.xlabel('Max List Value')
+                plt.ylabel('Number of Comparisons')
+                plt.show()
+                print("")
+
+            elif option == 4:
+                break
+
+            elif option == 5:
+                print("Goodbye!\n")
+                exit()
+
+            else:
+                print("Invalid input, try again.\n")
+
+    elif option == 3:
+        print("Goodbye!\n")
+        exit()
+    else:
+        print("Invalid input, try again.\n")
